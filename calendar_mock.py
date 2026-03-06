5f48ce6eb1d7e8ee1b32cee77669771f2bac4fee
Rodrigue Adebigni <rodadebigni@icloud.com>
2026-03-05 21:25:48 -0500

    Cleanup temporary scripts and temporary analytics files

--- calendar_mock.py
+++
@@ -1,6 +0,0 @@
-import calendar
-from datetime import datetime
-
-# Let's generate a simple calendar grid for March 2026
-c = calendar.monthcalendar(2026, 3)
-print(c)
--- generate_analytics.py
+++
@@ -1,1 +0,0 @@
-print("Preparing to rewrite AnalyticsPage...")
